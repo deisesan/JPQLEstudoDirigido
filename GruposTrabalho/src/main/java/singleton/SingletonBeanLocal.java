e56c5a5e3699836d3818e621caad71edfc8f6a22
Deise Santana <dsds3@aluno.ifnmg.edu.br>
2023-09-24 13:46:55 -0300

    criado singleton

---
+++ GruposTrabalho/src/main/java/singleton/SingletonBeanLocal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/SessionLocal.java to edit this template
+ */
+package singleton;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author deise
+ */
+@Local
+public interface SingletonBeanLocal {
+
+    void SingletonBean();
+
+}
